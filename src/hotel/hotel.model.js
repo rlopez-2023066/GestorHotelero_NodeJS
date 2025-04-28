40f12733cbeb6227e76a2adab52af9d6f899ef8c
pconsuegra-2023018 <pconsuegra-2023018@kinal.edu.gt>
2025-04-28 15:31:53 -0600

    Created hote.mode.js

---
+++ src/hotel/hotel.model.js
@@ -0,0 +1,33 @@
+import { model, Schema } from "mongoose";
+
+const hotelSchema = Schema(
+    {
+        name:{
+            type:String,
+            required: [true, 'Name is required'],
+            maxLength: [50, `Can't be overcome 50 characters`]
+        },
+        direction:{
+            type:String,
+            required: [true, 'Direction is required'],
+            maxLength: [90, `Can't be overcome 90 characters`]
+        },
+        category:{
+            type:Number,
+            max: [10, `Can't be overcome 10`],
+            min: [0, `Can't be less than 0 `]
+        },
+        description:{
+            type:String,
+            required: [true, 'Description is required'],
+            maxLength: [200, `Can't be overcome 200 characters`]
+        },
+        telephone:{
+            type:String,
+            required: [true, 'Telephone is required'],
+            maxLength: [14, `Can't be overcome 14 characters`]
+        }
+    }
+)
+
+export default model('Motel',hotelSchema)
