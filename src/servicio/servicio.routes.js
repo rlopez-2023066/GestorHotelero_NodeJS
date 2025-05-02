485f6be47fd252cde4ac1cd3f164f7ea434a46ee
JRetana-2023025 <jretana-2023025@kinal.edu.gt>
2025-05-02 10:36:32 -0600

    Update servicio.routes.js

--- src/servicio/servicio.routes.js
+++ src/servicio/servicio.routes.js
@@ -5,12 +5,13 @@ import {getAllServices,
      updateService, 
      deleteService } from './servicio.controller.js'
 import {validateJwt} from '../../middlewares/validate.jwt.js'
+import { serviceCreateValidator,serviceUpdateValidator} from '../../middlewares/validators.js'
 
 const api = Router()
 
 api.get('/getService',getAllServices)
-api.post('/addService', addService)
-api.put('/updateSerive/:id', validateJwt,updateService)
+api.post('/addService',validateJwt,serviceCreateValidator, addService)
+api.put('/updateSerive/:id', validateJwt,serviceUpdateValidator,updateService)
 api.delete('/deleteService/:id',validateJwt, deleteService)
 
 export default api
