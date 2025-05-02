b9d57e081fb534813bc9b783fae7f9deb3d4251d
Alfredo López <rlopez-2023066@kinal.edu.gt>
2025-05-01 19:19:21 -0600

    Update app.js

--- configs/app.js
+++ configs/app.js
@@ -9,6 +9,14 @@ import helmet from 'helmet'
 import cors from 'cors'
 import authRoutes from '../src/auth/auth.routes.js'
 import userRoutes from '../src/user/user.routes.js'
+import hotelRoutes from '../src/hotel/hotel.routes.js'
+import serviceRoutes from '../src/servicio/servicio.routes.js'
+import eventRoutes from '../src/evento/event.routes.js'
+import roomRoutes from '../src/habitacion/habitacion.routes.js'
+import reservationRouter from '../src/resevacion/reservacion.routes.js'
+import invoiceRouter from '../src/factura/factura.routes.js'
+
+
 
 const configs = (app) => {
     app.use(express.json())
@@ -25,6 +33,12 @@ const routes = (app) => {
     //Rutas de Autenticación
     app.use(authRoutes)
     app.use('/v1/user', userRoutes)
+    app.use('/v1/hotel',hotelRoutes)
+    app.use('/v1/service',serviceRoutes)
+    app.use('/v1/events', eventRoutes)
+    app.use('/v1/rooms',roomRoutes)
+    app.use('/v1/reservation', reservationRouter)
+    app.use('/v1/invoice', invoiceRouter)
     
 
 }
