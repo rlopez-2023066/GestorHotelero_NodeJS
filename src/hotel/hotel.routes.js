18fdd7aa96e64586103f0e8e33e585a08d22fd23
Alfredo López <rlopez-2023066@kinal.edu.gt>
2025-05-01 19:19:47 -0600

    Create hotel.routes.js

---
+++ src/hotel/hotel.routes.js
@@ -0,0 +1,24 @@
+import { Router } from "express";
+
+import { updateHotelValidator, registerHotelValidator } from "../../middlewares/validators.js";
+import { addHotel, getAllHotels, updateHotel, deleteHotel } from "./hotel.controller.js";
+import { isAdmin, validateJwt } from "../../middlewares/validate.jwt.js";
+
+
+const api = Router()
+
+//Agregar Hotel
+api.post('/addHotel', validateJwt, registerHotelValidator, addHotel)
+
+//Obtener Hoteles
+api.get('/getHotels', validateJwt, getAllHotels)
+
+//Actualizar Hotel
+api.put('/updateHotel/:id',validateJwt, updateHotelValidator,updateHotel)
+
+//Eliminar Hotel
+api.delete('/deleteHotel/:id',validateJwt, deleteHotel)
+
+
+
+export default api
