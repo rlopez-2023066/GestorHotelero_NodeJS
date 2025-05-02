a64212abe4882776975e8d7120c4d12711e0224d
Jose Estrada <jestrada-2023074@kinal.edu.gt>
2025-05-02 00:19:24 -0600

    Update event.routes.js

--- src/evento/event.routes.js
+++ src/evento/event.routes.js
@@ -7,7 +7,10 @@ import {
         getEventById, 
         updateEvent 
     } from './event.controller.js' //Import function of controller event
-
+import { 
+        updateHotelValidator,
+        createEventValidator
+    } from '../../middlewares/validators.js';
 import { validateJwt } from '../../middlewares/validate.jwt.js';
 
 
@@ -15,8 +18,8 @@ import { validateJwt } from '../../middlewares/validate.jwt.js';
 const api = Router() 
 
 //Routes from Event
-api.post('/addEvent', validateJwt, createEvent) //Create Event
-api.put('/updateEvent/:id', validateJwt, updateEvent) //Update Event
+api.post('/addEvent', validateJwt, createEventValidator, createEvent) //Create Event
+api.put('/updateEvent/:id', updateHotelValidator, validateJwt, updateEvent) //Update Event
 api.delete('/deleteEvent/:id', validateJwt, deleteEvent) //Delete Event
 api.get('/listEventAll', validateJwt, getAllEvents); //Get All Events
 api.get('/listEventById/:id', validateJwt, getEventById) //Get Event by id
