1bb06b7e2eb6d6fb5f676ec329dc11bc1d1ee2d4
pconsuegra-2023018 <pconsuegra-2023018@kinal.edu.gt>
2025-05-02 11:48:49 -0600

    Update hotel.routes.js

--- src/hotel/hotel.routes.js
+++ src/hotel/hotel.routes.js
@@ -1,7 +1,15 @@
 import { Router } from "express";
 
 import { updateHotelValidator, registerHotelValidator } from "../../middlewares/validators.js";
-import { addHotel, getAllHotels, updateHotel, deleteHotel } from "./hotel.controller.js";
+import { addHotel, 
+        getAllHotels, 
+        updateHotel, 
+        deleteHotel, 
+        withoutId,
+        getHotelsAtoZ,
+        getHotelsZtoA,
+        getHotelById,
+        geHoteltByName } from "./hotel.controller.js";
 import { isAdmin, validateJwt } from "../../middlewares/validate.jwt.js";
 
 
@@ -9,12 +17,29 @@ const api = Router()
 
 //Agregar Hotel
 api.post('/addHotel', validateJwt, registerHotelValidator, addHotel)
+
 //Obtener Hoteles
 api.get('/getHotels', validateJwt, getAllHotels)
+
+
+api.get('/getHotels/ZA',validateJwt,getHotelsZtoA)
+
+api.get('/getHotels/AZ',validateJwt,getHotelsAtoZ)
+
+
+api.get('/getHotels/:id',validateJwt,getHotelById)
+
+api.get('/get',validateJwt,geHoteltByName)
+
 //Actualizar Hotel
 api.put('/updateHotel/:id',validateJwt,updateHotelValidator,updateHotel)
+
+api.put('/updateHotel/',validateJwt,withoutId)
+
 //Eliminar Hotel
 api.delete('/deleteHotel/:id',validateJwt,deleteHotel)
+
+api.delete('/deleteHotel/',validateJwt,withoutId)
 
 
 
