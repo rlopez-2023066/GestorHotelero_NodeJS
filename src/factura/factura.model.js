bf11d215d7195bd71a8506b77a4a7e2e88d78ef2
Alfredo López <rlopez-2023066@kinal.edu.gt>
2025-05-01 19:19:34 -0600

    Create factura.model.js

---
+++ src/factura/factura.model.js
@@ -0,0 +1,53 @@
+import {Schema, model} from 'mongoose'
+
+const invoiceSchema = new Schema(
+    {
+        reservation: {
+            type: Schema.Types.ObjectId,
+            ref: 'Reservation',
+            required: true
+        },
+
+        user: {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+            required: true
+        },
+
+        hotel: {
+            type: Schema.Types.ObjectId,
+            ref: 'Hotel',
+            required: true
+        },
+
+        issueDate: {
+            type: Date,
+            required: true,
+            default: Date.now
+        },
+        
+        tax: {
+            type: Number,
+            required: true
+        },
+
+        total:{
+            type: Number,
+            required: true
+        },
+
+        status: {
+            type: String,
+            enum: ['paid', 'unpaid'],
+            default: 'unpaid'
+        },
+
+
+    },
+    {
+        versionKey: false, //Deshabilitar el __v(Versión del documento)
+        timestamps: true //Agrega propiedades de fecha (Fecha de creación y de ultima actualización)
+    }
+)
+
+export default model('Invoice', invoiceSchema)
