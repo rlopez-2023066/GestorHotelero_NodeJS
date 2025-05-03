df77037a13fa1455ca12c8c99669cd4ff6777a02
dmarroquin-2023076 <dmarroquin-2023076@kinal.edu.gt>
2025-05-02 21:50:41 -0600

    Update habitacion.routes.js

--- src/habitacion/habitacion.routes.js
+++ src/habitacion/habitacion.routes.js
@@ -7,23 +7,22 @@ import {
     deleteRoom 
 } from "./habitacion.controller.js";
 
+import { validateJwt } from "../../middlewares/validate.jwt.js";
+import { deleteRoomValidator, saveRoomValidator, updateRoomValidator } from "../../middlewares/validators.js";
+
+
+
 const api = Router();
 
-api.get('/getRoom', getAll);
-api.get('/:id', getRoomById);
-api.post(
-    '/',
-    saveRoom
-)
-
-api.put(
-    '/:id',
-    updateRoom
-)
-
-api.delete(
-    '/delete/:id',
-    deleteRoom
-)
+//Buscar todos las habitaciones
+api.get('/getRoom', validateJwt, getAll);
+//Buscar habitacion por ID
+api.get('/getRoomById/:id', validateJwt,getRoomById);
+//Agregar habitacion
+api.post('/addRoom',saveRoomValidator, validateJwt, saveRoom)
+//Actualizar habitacion
+api.put('/updateRoom/:id',updateRoomValidator, validateJwt, updateRoom)
+//Eliminar habitacion
+api.delete('/deleteRoom/:id',deleteRoomValidator,validateJwt, deleteRoom)
 
 export default api;
