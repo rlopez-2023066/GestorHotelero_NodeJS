1b52b844324d95bdda0c379951b596e51ea7b7ec
Alfredo López <rlopez-2023066@kinal.edu.gt>
2025-05-04 19:52:16 -0600

    Update hotel.controller.js

--- src/hotel/hotel.controller.js
+++ src/hotel/hotel.controller.js
@@ -5,7 +5,10 @@ import Hotel from './hotel.model.js'
 //Agregar Hotel
 export const addHotel = async(req,res) =>{
     try {
+
+        
         const data = req.body
+        
 
         const hotel = new Hotel(data)
 
@@ -123,6 +126,128 @@ export const deleteHotel = async(req,res)=>{
             }
         )
 
+    } catch (error) {
+        console.error(error)
+        return res.status(500).send(
+            {
+                success: false,
+                message: 'Internal  error',
+                error
+            }
+        )
+    }
+}
+
+//Obtener Hoteles por orden alfabético Z-A
+export const getHotelsAtoZ = async(req,res)=>{
+    try {
+        const hotels = await Hotel.find().sort({name: 1})
+
+        if(!hotels){
+            return res.status(404).send(
+                {
+                    success: false,
+                    message: 'Could not found Hotels '
+                }
+            )
+        }
+
+        return res.status(200).send(
+            {
+                success: true,
+                message: 'Hotels found: ',
+                hotel: hotels
+            }
+        )
+    } catch (error) {
+        console.error(error)
+        return res.status(500).send(
+            {
+                success: false,
+                message: 'Internal  error',
+                error
+            }
+        )
+    }
+}
+
+//Obtener Hoteles por orden alfabético A-Z
+export const getHotelsZtoA = async(req,res)=>{
+    try {
+        const hotels = await Hotel.find().sort({name: -1})
+
+        if(!hotels){
+            return res.status(404).send(
+                {
+                    success: false,
+                    message: 'Could not found Hotels '
+                }
+            )
+        }
+
+        return res.status(200).send(
+            {
+                success: true,
+                message: 'Hotels found: ',
+                hotel: hotels
+            }
+        )
+    } catch (error) {
+        console.error(error)
+        return res.status(500).send(
+            {
+                success: false,
+                message: 'Internal  error',
+                error
+            }
+        )
+    }
+}
+
+//Obtener Hotel por ID
+export const getHotelById = async(req,res)=>{
+    try {
+        const {id} = req.params
+
+        const hotels = await Hotel.findById(id)
+
+        if(!hotels){
+            return res.status(404).send(
+                {
+                    success: false,
+                    message: 'Could not found Hotels '
+                }
+            )
+        }
+
+        return res.status(200).send(
+            {
+                success: true,
+                message: 'Hotels found: ',
+                hotel: hotels
+            }
+        )
+    } catch (error) {
+        console.error(error)
+        return res.status(500).send(
+            {
+                success: false,
+                message: 'Internal  error',
+                error
+            }
+        )
+    }
+}
+
+//Mensage de error para cuando no se envia el ID
+export const withoutId = async(req,res)=>{
+    try {
+        return res.status(404).send(
+            {
+                success: false,
+                message: 'Please entre an Id'
+            }
+        )
     } catch (error) {
         console.error(error)
         return res.status(500).send(
