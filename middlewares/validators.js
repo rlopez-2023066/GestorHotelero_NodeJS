8fe7cbefd332eb8d8d25af3adc861a97d4666610
Alfredo López <rlopez-2023066@kinal.edu.gt>
2025-05-04 19:51:49 -0600

    Update validators.js

--- middlewares/validators.js
+++ middlewares/validators.js
@@ -8,10 +8,20 @@ import {
 } from './validate.error.js'
 
 import {
+    existName,  
     existEmail,
+    existRoomNumberInHotel,
     existUsername,
-    notRequiredField
+    isValidHotelId,
+    notRequiredField,
+    preventRoomNumberUpdate,
+    validateNoEmptyUpdates,
+    serviceExists,
+    duplicateServiceHotel,
+    duplicateServiceHotelUpdate
 } from '../utils/db.validators.js'
+
+
 
 /* Observación: Colocar comentario acerca de la Validación */
 
@@ -44,12 +54,12 @@ export const registerValidator = [
 
 //Validaciones para que el usuario actualice su perfil.
 export const updateUserValidator = [
-    body('username', 'Name cannot be empty')
+    body('username')
         .optional()
         .notEmpty()
         .toLowerCase()
         .custom((username, {req}) => existUsername(username, req.user)),
-    body('email', 'Email cannot be empty')
+    body('email')
         .optional()
         .notEmpty()
         .isEmail()
@@ -91,7 +101,8 @@ export const registerHotelValidator = [
     body('name')
         .notEmpty()
         .isLength({min: 8, max: 50 })
-        .withMessage('Enter name again'),
+        .withMessage('Enter name again')
+        .custom(existName),
     body('direction')
         .notEmpty()
         .isLength({min: 5, max: 90})
@@ -105,28 +116,35 @@ export const registerHotelValidator = [
     body('telephone')
         .notEmpty()
         .isMobilePhone(),
-    validateErrorsWithoutFiles
+    validateErrors
 ]
 
-//Validciones para actualizar el Hotel
+//Validaciones para actualizar el Hotel
 export const updateHotelValidator = [
     body('name')
         .optional()
-        .notEmpty(),
+        .notEmpty()
+        .withMessage('Enter a valid name')
+        .custom((name,{req})=> existName(name, req.params?.id)),
     body('direction')
         .optional()
-        .notEmpty(),
+        .notEmpty()
+        .withMessage('Enter a valid direction'),
     body('category')
         .optional()
-        .notEmpty(),
+        .notEmpty()
+        .withMessage('Enter a valid category'),
     body('description')
         .optional()
-        .notEmpty(),
+        .notEmpty()
+        .withMessage('Enter a valid description'),
     body('telephone')
         .optional()
-        .notEmpty(),
+        .notEmpty()
+        .withMessage('Enter a valid telephone'),
     validateErrorsWithoutFiles
 ]
+
 
 //Validaciones para crear una reservación de Habitación 
 export const registerReservationValidator = [
@@ -171,6 +189,270 @@ export const registerEventValidator = [
     
 ]
 
+//validaciones para agregar servicios
+export const serviceCreateValidator = [
+    body('name')
+    .trim()
+    .notEmpty()
+    .withMessage("Service name's is required")
+    .isLength({min: 3}).withMessage('The name must have at least 3 characters')
+    .isLength({max: 20}).withMessage('The name cannot have more than 20 characters')
+    .custom(duplicateServiceHotel),
+
+    body('amount')
+    .notEmpty()
+    .withMessage('Amount is required'),
+
+    body('priceOne')
+    .notEmpty().withMessage('Price is required')
+    .isNumeric().withMessage('Price got to be number'),
+
+    body('description')
+    .notEmpty()
+    .withMessage("Description is required").withMessage('Description cannot have more than 20 characters')
+    .isLength({max: 300}),
+
+    body('hotel')
+    .notEmpty()
+    .withMessage('Hotel ID is required')
+    .isMongoId().withMessage('Invalid Hotel ID format'),
+
+    validateErrorsWithoutFiles
+]
+//validaciones para actualizar serivicio
+export const serviceUpdateValidator = [
+    body('name')
+    .optional()
+    .notEmpty()
+    .withMessage("Service name's is required")
+    .isLength({min: 3}).withMessage('The name must have at least 3 characters')
+    .isLength({max: 20}).withMessage('The name cannot have more than 20 characters')
+    .custom(duplicateServiceHotelUpdate),
+
+    body('amount')
+    .optional()
+    .notEmpty().withMessage('Amount is required')
+    .isNumeric().withMessage('Amount got to be number'),
+
+    body('priceOne')
+    .optional()
+    .notEmpty().withMessage('Price is required')
+    .isNumeric().withMessage('Price got to be number'),
+
+    body('description')
+    .optional()
+    .notEmpty().withMessage("Description is required").withMessage('Description cannot have more than 20 characters')
+    .isLength({max: 300}),
+
+    body('hotel')
+    .optional()
+    .notEmpty()
+    .custom(notRequiredField),
+
+    
+
+    validateErrorsWithoutFiles
+]
+
+//Validaciones para agregar HABITACION
+export const saveRoomValidator = [
+    body('no_room')
+        .notEmpty().withMessage('Room number is required')
+        .isNumeric().withMessage('Room number must be a number')
+        .toInt() 
+        .custom(existRoomNumberInHotel)
+        .withMessage('Room number already exists in this hotel'),
+    
+    body('type')
+        .notEmpty().withMessage('Room type is required')
+        .isIn(['Individual', 'Double', 'Double Deluxe', 'Presidential Suite', 'Living_room'])
+        .withMessage('Invalid room type'),
+    
+    body('description')
+        .optional()
+        .isLength({ max: 200 })
+        .withMessage('Description cannot exceed 200 characters'),
+    
+    body('capacity')
+        .notEmpty().withMessage('Capacity is required')
+        .isInt({ min: 1 }).withMessage('Capacity must be at least 1'),
+    
+    body('price')
+        .notEmpty().withMessage('Price is required')
+        .isFloat({ min: 0 }).withMessage('Price cannot be negative'),
+    
+    body('hotel')
+        .notEmpty().withMessage('Hotel ID is required')
+        .isMongoId().withMessage('Invalid Hotel ID format')
+        .custom(isValidHotelId)
+        .withMessage('Hotel does not exist'),
+
+    body('state')
+        .optional()
+        .notEmpty()
+        .custom(notRequiredField),
+    
+    body('sales')
+        .optional() 
+        .notEmpty()
+        .custom(notRequiredField),
+    
+    validateErrors
+]
+
+//Validaciones para actualizar HABITACION
+export const updateRoomValidator = [
+    body()
+        .custom(validateNoEmptyUpdates),
+
+    body('no_room')
+        .optional()
+        .custom(preventRoomNumberUpdate),
+
+    body('type')
+        .optional()
+        .isIn(['Individual', 'Double', 'Double Deluxe', 'Presidential Suite', 'Living_room'])
+        .withMessage('Invalid room type'),
+
+    body('description')
+        .optional()
+        .isLength({ max: 200 })
+        .withMessage('Description cannot exceed 200 characters'),
+
+    body('capacity')
+        .optional()
+        .isInt({ min: 1 })
+        .withMessage('Capacity must be at least 1'),
+
+    body('state')
+        .optional()
+        .isIn(['available', 'busy'])
+        .withMessage('Invalid room state'),
+
+    
+    body('price')
+        .optional()
+        .isFloat({ min: 0 })
+        .withMessage('Price cannot be negative'),
+    
+   
+    
+    validateErrors
+]
 
 
 
+export const createEventValidator = [
+    body('name', 'Name is required')
+      .notEmpty()
+      .isLength({ min: 3 }),
+  
+    body('description', 'Description is required')
+      .notEmpty()
+      .isLength({ min: 10 }),
+  
+    body('type', 'Type is required')
+      .notEmpty(),
+  
+    body('startTime', 'Start time is required and must be a valid date')
+      .notEmpty()
+      .isISO8601(),
+  
+    body('endTime', 'End time is required and must be a valid date')
+      .notEmpty()
+      .isISO8601()
+      .custom((endTime, { req }) => {
+        if (new Date(endTime) <= new Date(req.body.startTime)) {
+          throw new Error('End time must be after start time')
+        }
+        return true
+      }),
+  
+    body('services', 'Services must be an array')
+      .isArray(),
+  
+    body('services.*.serviceId', 'Invalid service ID')
+      .isMongoId()
+      .custom(serviceExists),
+  
+    body('services.*.quantity', 'Quantity must be a positive integer')
+      .isInt({ min: 1 }),
+
+    body('hotel')
+        .notEmpty()
+        .withMessage('Hotel ID is required'),
+    
+    body('room')
+      .notEmpty()
+      .withMessage('Room ID is required'),
+
+    body('user')
+        .notEmpty()
+        .withMessage('User ID is required'),
+    
+    validateErrorsWithoutFiles
+  ]
+
+
+  export const updateEventValidator = [
+    body('name')
+      .optional()
+      .isLength({ min: 3 })
+      .withMessage('Name must be at least 3 characters'),
+  
+    body('description')
+      .optional()
+      .isLength({ min: 10 })
+      .withMessage('Description must be at least 10 characters'),
+  
+    body('type')
+      .optional()
+      .notEmpty()
+      .withMessage('Type cannot be empty'),
+  
+    body('startTime')
+      .optional()
+      .isISO8601()
+      .withMessage('Start time must be a valid date'),
+  
+    body('endTime')
+      .optional()
+      .isISO8601()
+      .withMessage('End time must be a valid date')
+      .custom((endTime, { req }) => {
+        if (req.body.startTime && new Date(endTime) <= new Date(req.body.startTime)) {
+          throw new Error('End time must be after start time')
+        }
+        return true
+      }),
+  
+    body('services')
+      .optional()
+      .isArray()
+      .withMessage('Services must be an array'),
+  
+    body('services.*.serviceId')
+      .optional()
+      .isMongoId()
+      .withMessage('Invalid service ID')
+      .custom(serviceExists),
+  
+    body('services.*.quantity')
+      .optional()
+      .isInt({ min: 1 })
+      .withMessage('Quantity must be at least 1'),
+  
+    validateErrorsWithoutFiles
+]
+
+export const createInvoiceValidator = [
+    body('event')
+        .notEmpty()
+        .withMessage('Event ID is required')
+        .isMongoId()
+        .withMessage('Invalid Event ID format'),
+    body('reservation')
+        .optional()
+        .custom(notRequiredField),
+    validateErrorsWithoutFiles
+]
