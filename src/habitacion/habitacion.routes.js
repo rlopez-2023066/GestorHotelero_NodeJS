d5d85baff6e6e703cbe2f8039bab605e09f73b1e
Alfredo López <rlopez-2023066@kinal.edu.gt>
2025-05-04 19:52:15 -0600

    Update habitacion.routes.js

--- src/habitacion/habitacion.routes.js
+++ src/habitacion/habitacion.routes.js
@@ -1,27 +1,37 @@
-import { Router } from "express" 
+import { Router } from "express";
 import { 
     saveRoom, 
     getAll, 
     getRoomById, 
     updateRoom, 
-    deleteRoom 
-} from "./habitacion.controller.js" 
+    deleteRoom,
+    getRoomByState
+} from "./habitacion.controller.js";
 
-import { validateJwt } from "../../middlewares/validate.jwt.js" 
+import { validateJwt, authorizeRoles} from "../../middlewares/validate.jwt.js";
+import { saveRoomValidator, updateRoomValidator } from "../../middlewares/validators.js";
 
 
 
-const api = Router() 
+const api = Router();
 
-//Buscar todos los Hoteles
-api.get('/getRoom', validateJwt, getAll) 
-//Buscar Hotel por ID
-api.get('/getRoomById/:id', validateJwt,getRoomById) 
-//Agregar Hotel
-api.post('/addRoom', validateJwt, saveRoom)
-//Actualizar Hotel
-api.put('/updateRoom/:id', validateJwt, updateRoom)
-//Eliminar Hotel
+//Role Cliente
+//Buscar todos las habitaciones
+api.get('/getRoom', validateJwt , authorizeRoles('ADMIN_HOTEL','CLIENT'), getAll);
+//Buscar habitacion por ID
+api.get('/getRoomById/:id', validateJwt, authorizeRoles('ADMIN_HOTEL', 'CLIENT'), getRoomById);
+//Buscar habitacion no reservadas
+api.get('/getRoomByState', validateJwt, authorizeRoles('ADMIN_HOTEL','CLIENT'), getRoomByState);
+
+
+
+
+//Role Admin_Hotel
+//Agregar habitacion
+api.post('/addRoom',saveRoomValidator, validateJwt, saveRoom)
+//Actualizar habitacion
+api.put('/updateRoom/:id',updateRoomValidator, validateJwt, updateRoom)
+//Eliminar habitacion
 api.delete('/deleteRoom/:id',validateJwt, deleteRoom)
 
-export default api 
+export default api;
