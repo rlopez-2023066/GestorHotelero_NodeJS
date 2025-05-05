1444735fe8855eceac5bbcce3639762d5a003f15
Alfredo López <rlopez-2023066@kinal.edu.gt>
2025-05-04 19:52:05 -0600

    Update event.controller.js

--- src/evento/event.controller.js
+++ src/evento/event.controller.js
@@ -1,152 +1,220 @@
 import Event from "./event.model.js"
 import Service from "../servicio/servicio.model.js"
+import Room from '../habitacion/habitacion.model.js'
 
-//Create Event
+
+// Create Event
 export const createEvent = async (req, res) => {
-    try {
-      const data = req.body 
-  
-      const servicesArray = [] 
-  
-      for (const item of data.services) {
-        const service = await Service.findById(item.serviceId) 
-  
-        if (!service) {
-          return res.status(404).send({
-            success: false,
-            message: `Service not found`
-          }) 
-        }
-  
-        if (service.amount < item.quantity) {
-          return res.status(400).send({
-            success: false,
-            message: `Not enough quantity for Service`
-          }) 
-        }
-  
-        service.amount -= item.quantity 
-  
-        await service.save() 
-  
-        servicesArray.push({
-          serviceId: item.serviceId,
-          quantity: item.quantity,
-          price: service.priceOne
-        }) 
-      }
-  
-      data.services = servicesArray 
-  
-      const event = new Event(data) 
-      await event.save() 
-  
-      return res.send({
-        success: true,
-        message: 'Event created successfully',
-        event
-      }) 
+  try {
 
-    } catch (err) {
-      console.error('General error', err) 
-      return res.status(500).send({
-        success: false,
-        message: 'General error',
-        err
-      }) 
-    }
-  } 
 
-//Update Event
-export const updateEvent = async (req, res) => {
-    try{
-        const { id } = req.params
-        const data = req.body
+    const data = req.body  
 
-        //Validate that the service exists
-        if(!data.service){
-            const serviceExist = await Service.findById(data.services)
-            if(!serviceExist){
-                return res.status(404).send({
-                    success: false,
-                    message: 'Service not found'
-                })
-            }
-        }
-        //Update Event
-        const updateEvent = await Event.findByIdAndUpdate(
-            id,
-            data,
-            { new:true }
-        ).populate("services")
-
-        if(!updateEvent){
-            return res.status(404).send({
+    let hotelIdJwt = req.user.hotel
+    
+    if(!hotelIdJwt){
+        return res.status(403).send(
+            {
                 success: false,
-                message: 'Event not found and not updated'
-            })
-        }
-
-        return res.send({
-            success: true,
-            message: 'Event updated succesfully',
-            event: updateEvent
-        })
-    } catch (err){
-        console.error('General error', err) 
-        return res.status(500).send({
-            success: false,
-            message: 'General error',
-            err
-        })
+                message: 'You are not the hotel manager.'
+            }
+        )
     }
-}
+    
+    if( data.hotel.toString() !== hotelIdJwt){
+        return res.status(403).send(
+            {
+                success: false,
+                message: 'You do not have permission to modify this service'
+            }
+        )
+    }
+
+
+    const servicesArray = []  
+
+    // Verificar si ya existe un evento con la misma fecha y hora
+    const existingEvent = await Event.findOne({
+      startTime: data.startTime,
+      endTime: data.endTime
+    })  
+
+    if (existingEvent) {
+      return res.status(400).send({
+        success: false,
+        message: 'An event with the same date and time already exists.'
+      })  
+    }
+
+    const room = await Room.findById(data.room)  
+
+    //Validar que la habitación exista
+    if (!room) {
+      return res.status(404).send({
+        success: false,
+        message: 'Room not found'
+      })  
+    }
+
+    if (String(room.hotel) !== String(data.hotel)) {
+      return res.status(403).send({
+        success: false,
+        message: 'Room does not belong to the selected hotel'
+      })  
+    }
+
+    if (room.type !== 'Living_room') {
+      return res.status(400).send({
+        success: false,
+        message: 'Only rooms of type "Living_room" can be used for events'
+      })  
+    }
+    console.log(room.type);
+
+    if(room.state !== 'available') {
+      return res.status(400).send(
+        {
+          success: false, 
+          message: 'Room is not available'
+        }
+      )
+    }
+
+    // Validar que todos los servicios pertenezcan al hotel
+    for (const item of data.services) {
+      const service = await Service.findById(item.serviceId)  
+
+      if (!service) {
+        return res.status(404).send({
+          success: false,
+          message: `Service not found`
+        })  
+      }
+
+      if (String(service.hotel) !== String(data.hotel)) {
+        return res.status(403).send({
+          success: false,
+          message: `Service ${service.name} does not belong to the selected hotel`
+        })  
+      }
+
+      if (service.amount < item.quantity) {
+        return res.status(400).send({
+          success: false,
+          message: `Not enough quantity for Service: ${service.name}`
+        })  
+      }
+
+      service.amount -= item.quantity  
+      await service.save()  
+
+      servicesArray.push({
+        serviceId: item.serviceId,
+        quantity: item.quantity,
+        price: service.priceOne
+      })  
+    }
+
+    data.services = servicesArray  
+
+    room.state = 'busy'
+    await room.save()
+
+    const event = new Event(data)  
+    await event.save()  
+
+    return res.send({
+      success: true,
+      message: 'Event created successfully',
+      event
+    })  
+
+  } catch (err) {
+    console.error('General error', err)  
+    return res.status(500).send({
+      success: false,
+      message: 'General error',
+      err
+    })  
+  }
+}  
+
+
+
 
 //Delete Event
 export const deleteEvent = async (req, res) => {
-    try {
-      const { id } = req.params 
+  try {
+
+      
+    const { id } = req.params 
+    
+    const eventData = await Event.findById(id) 
+    const roomData = await Room.findById(eventData.room)
+
+    let hotelIdJwt = req.user.hotel
   
-      const event = await Event.findById(id) 
+    if(!hotelIdJwt){
+        return res.status(403).send(
+            {
+                success: false,
+                message: 'You are not the hotel manager.'
+            }
+        )
+    }
+    
+    if( eventData.hotel.toString() !== hotelIdJwt){
+        return res.status(403).send(
+            {
+                success: false,
+                message: 'You do not have permission to modify this service'
+            }
+        )
+    }
   
-      if (!event) {
-        return res.status(404).send({
-          success: false,
-          message: 'Event not found'
-        }) 
-      }
   
-      for (const item of event.services) {
-        const service = await Service.findById(item.serviceId) 
-  
-        if (!service) {
-          return res.status(404).send({
-            success: false,
-            message: `Service with ID ${item.serviceId} not found`
-          }) 
-        }
-  
-        service.amount += item.quantity 
-  
-        await service.save() 
-      }
-  
-      await Event.findByIdAndDelete(id) 
-  
-      return res.send({
-        success: true,
-        message: 'Event removed successfully'
-      }) 
-  
-    } catch (err) {
-      console.error('General error', err) 
-      return res.status(500).send({
+    if (!eventData) {
+      return res.status(404).send({
         success: false,
-        message: 'General error',
-        err
+        message: 'Event not found'
       }) 
     }
-  } 
+
+    for (const item of eventData.services) {
+      const service = await Service.findById(item.serviceId) 
+
+      if (!service) {
+        return res.status(404).send({
+          success: false,
+          message: `Service with ID ${item.serviceId} not found`
+        }) 
+      }
+
+      service.amount += item.quantity 
+
+      await service.save() 
+    }
+
+    roomData.state = 'available'
+    await roomData.save()
+
+
+    await Event.findByIdAndDelete(id) 
+
+    return res.send({
+      success: true,
+      message: 'Event removed successfully'
+    }) 
+
+  } catch (err) {
+    console.error('General error', err) 
+    return res.status(500).send({
+      success: false,
+      message: 'General error',
+      err
+    }) 
+  }
+} 
 
 //Get all Events
 export const getAllEvents = async (req, res) => {
@@ -157,17 +225,17 @@ export const getAllEvents = async (req, res) => {
             .skip(Number(skip))
             .limit(Number(limit))
 
-            if (event.length ===0) {
-                return res.status(404).send({
-                    success: false,
-                    message:  'Event not found'
-                })
-            }
-            return res.send({
-                success: true,
-                message:  'Event found:',
-                event
-            })
+      if (event.length ===0) {
+          return res.status(404).send({
+              success: false,
+              message:  'Event not found'
+          })
+      }
+        return res.send({
+            success: true,
+            message:  'Event found:',
+            event
+        })
     }catch (err){
         console.error('General error', err) 
         return res.status(500).send({
@@ -180,29 +248,62 @@ export const getAllEvents = async (req, res) => {
 
 //Get by ID Event
 export const getEventById = async (req, res) => {
-    try{
-        const { id } = req.params
-        const event = await Event.findById(id)
-            .populate("services")
-        
-        if(!event){
-            return res.status(404).send({
-                success: false,
-                message: 'Event not found'
-            })
-        }
-        return res.send({
-            success: true,
-            message: 'Event found:',
-            data: event
-        })
-    }catch (err){
-        console.error('General error', err) 
-        return res.status(500).send({
-            success: false,
-            message: 'General error',
-            err
-        })
-    }
+  try{
+      const { id } = req.params
+      const event = await Event.findById(id)
+      .populate("services")
+      
+      if(!event){
+          return res.status(404).send({
+              success: false,
+              message: 'Event not found'
+          })
+      }
+      
+      return res.send({
+          success: true,
+          message: 'Event found:',
+          data: event
+      })
+  }catch (err){
+      console.error('General error', err) 
+      return res.status(500).send({
+          success: false,
+          message: 'General error',
+          err
+      })
+  }
+}
+
+//Get Event by Hotel
+export const getEventByHotel = async (req, res) => {
+  try{
+      const { hotel } = req.body
+
+      const event = await Event.find({ hotel })
+      .populate("services")
+      
+      if(!event){
+          return res.status(404).send({
+              success: false,
+              message: 'Event not found'
+          })
+      }
+      
+      return res.send({
+          success: true,
+          message: 'Event found:',
+          data: event
+      })
+
+      
+  }catch (err){
+      console.error('General error', err) 
+      return res.status(500).send({
+          success: false,
+          message: 'General error',
+          err
+      })
+  }
 }
 
