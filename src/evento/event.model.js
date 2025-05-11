6f011bbe1ae944c6e6c65c12c7365612529dcce7
Jose Estrada <jestrada-2023074@kinal.edu.gt>
2025-05-11 17:57:48 -0600

    Update event.model.js

--- src/evento/event.model.js
+++ src/evento/event.model.js
@@ -1,41 +1,79 @@
 // model of event
-import mongoose, {Schema, model} from "mongoose";
+import mongoose, {Schema, model} from "mongoose" 
 
 const EventSchema = new mongoose.Schema({
-  description: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  name: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  type: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  startTime: {
-    type: Date,
-    required: true
-  },
-  endTime: {
-    type: Date,
-    required: true
-  },
-  status: {
-    type: String,
-    enum: ['Scheduled', 'Canceled', 'Finished'],
-    default: 'Scheduled'
-  },
-  services: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Service'
-  }]
+    name: {
+      type: String,
+      required: true,
+      trim: true
+    },
+
+    description: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    
+    type: {
+      type: String,
+      required: true,
+      trim: true
+    },
+
+    startTime: {
+      type: Date,
+      required: true
+    },
+
+    endTime: {
+      type: Date,
+      required: true
+    },
+
+    status: {
+      type: String,
+      enum: ['Scheduled', 'Canceled', 'Finished', 'In Progress', 'Completed'],
+      default: 'Scheduled'
+    },
+
+    hotel: {
+      type: Schema.Types.ObjectId,
+      ref: 'Hotel', 
+      required: true
+    },
+
+    room: {
+      type: Schema.Types.ObjectId,
+      ref: 'Room',
+      required: true
+    },
+
+    user:{
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
+    },
+    
+    services: [
+      {
+        serviceId:{
+          type: Schema.Types.ObjectId,
+          ref: 'Service',
+        },
+        quantity: {
+          type: Number,
+          required: true
+        },
+
+        price: {
+          type: Number
+        }
+      }
+    ],
+
+
 }, {
   timestamps: true
 })
 
-export default model('Event', EventSchema);
+export default model('Event', EventSchema) 
