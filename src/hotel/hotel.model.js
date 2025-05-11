e0f492a9d41e583d8d10039fb3847ed3d5191415
Jose Estrada <jestrada-2023074@kinal.edu.gt>
2025-05-11 17:58:35 -0600

    Update hotel.model.js

--- src/hotel/hotel.model.js
+++ src/hotel/hotel.model.js
@@ -1,11 +1,12 @@
-import { model, Schema } from "mongoose";
+import { model, Schema } from "mongoose" 
 
 const hotelSchema = Schema(
     {
         name:{
             type:String,
             required: [true, 'Name is required'],
-            maxLength: [50, `Can't be overcome 50 characters`]
+            maxLength: [50, `Can't be overcome 50 characters`],
+            unique: true
         },
         direction:{
             type:String,
@@ -27,7 +28,11 @@ const hotelSchema = Schema(
             required: [true, 'Telephone is required'],
             maxLength: [14, `Can't be overcome 14 characters`]
         }
+    },
+    {
+        versionKey: false, 
+        timestamps: true 
     }
 )
 
-export default model('Motel',hotelSchema)
+export default model('Hotel',hotelSchema)
