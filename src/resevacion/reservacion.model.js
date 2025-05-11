300b717e1fb8b954aa9ca84951d0b1e703a2ac48
Jose Estrada <jestrada-2023074@kinal.edu.gt>
2025-05-11 17:58:24 -0600

    Create reservacion.model.js

---
+++ src/resevacion/reservacion.model.js
@@ -0,0 +1,58 @@
+import {model, Schema} from 'mongoose'
+
+
+const reservationSchema = Schema(
+    {
+        user: {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+            required: true
+        },
+
+        room: {
+            type: Schema.Types.ObjectId,
+            ref: 'Room',
+            required: true
+        },
+
+        hotel: {
+            type: Schema.Types.ObjectId,
+            ref: 'Hotel',
+            required: true
+        },
+
+
+        description:{
+            type: String,
+            required: true
+
+        },
+
+        startTime: {
+            type: Date,
+            required: true
+        },
+
+        endTime: {
+            type: Date,
+            required: true
+        },
+        
+        status: {
+            type: String,
+            enum: ['pending', 'confirmed', 'cancelled'],
+            default: 'pending'
+        },
+
+        price: {
+            type: Number,
+            required: true
+        }
+    },
+    {
+        versionKey: false, //Deshabilitar el __v(Versión del documento)
+        timestamps: true //Agrega propiedades de fecha (Fecha de creación y de ultima actualización)
+    }
+)
+
+export default model('Reservation', reservationSchema)
