85528b21fece4ceb3445709f94dce92750a970b0
Alfredo López <rlopez-2023066@kinal.edu.gt>
2025-05-21 22:43:47 -0600

    Update validate.jwt.js

--- middlewares/validate.jwt.js
+++ middlewares/validate.jwt.js
@@ -4,33 +4,39 @@ import jwt from 'jsonwebtoken'
 import { findUser } from '../utils/db.validators.js'
 
 
-export const validateJwt = async(req, res, next)=>{
-    try{
-        //Obtener la llave de acceso privada al Token
-        let secretKey = process.env.SECRET_KEY
-        //Obtener el token de los headers (cabeceras)
-        let { authorization } = req.headers
-        //Verificamos que venga el token
-        if(!authorization) return res.status(401).send({message: 'Unauthorized'})
-        let user = jwt.verify(authorization, secretKey)
-        //Validar que el usuario exista
-        const validateUser = await findUser(user.uid)   
-        if(!validateUser) return res.status(404).send(
-            {
-                success: false,
-                message: 'User not found - unauthorized'
-            }
-        )
-        //Inyectar en la solicitud un nuevo parámetro
-        req.user = user
-        next()
-    }catch(err){
-        console.error(err)
-        return res.status(401).send({message: 'Invalid credentials'})
-    }
-}
+export const validateJwt = async (req, res, next) => {
+  try {
+    let secretKey = process.env.SECRET_KEY;
+    let { authorization } = req.headers;
 
-//Verificar si el usuario tiene el rol de Cliente
+    if (!authorization) {
+      return res.status(401).send({ message: 'Unauthorized' });
+    }
+
+    if (!authorization.startsWith('Bearer ')) {
+      return res.status(401).send({ message: 'Invalid token format' });
+    }
+    const token = authorization.split(' ')[1];
+
+    let user = jwt.verify(token, secretKey);
+
+    const validateUser = await findUser(user.uid);
+    if (!validateUser) {
+      return res.status(404).send({
+        success: false,
+        message: 'User not found - unauthorized',
+      });
+    }
+
+    req.user = user;
+    next();
+  } catch (err) {
+    console.error(err);
+    return res.status(401).send({ message: 'Invalid credentials' });
+  }
+};
+
+
 export const isClient = async(req, res, next)=>{
     try{
         const { user } = req
@@ -53,7 +59,6 @@ export const isClient = async(req, res, next)=>{
 }
 
 
-//Verifica si el usuario tiene el ROL de ADMIN en su usuario.
 export const isAdmin = async(req, res, next)=>{
     try{
         const { user } = req
@@ -96,10 +101,9 @@ export const isAdminHotel = async(req, res, next)=>{
     }
 }
 
-// middlewares/authorizeRoles.js
 export const authorizeRoles = (...allowedRoles) => {
     return (req, res, next) => {
-        const user = req.user  // Asumiendo que ya tienes el user en req (por ejemplo, desde JWT middleware)
+        const user = req.user  
 
         if (!user || !allowedRoles.includes(user.role)) {
             return res.status(403).json({
